1ed67327fc6f28b7d964e0818ba85ec3908dae42
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-09-11 19:12:54 +0800

    add: doc to core/Node.js

--- src/core/Node.js
+++ src/core/Node.js
@@ -1,6 +1,28 @@
+/**
+ * A node in grid. 
+ * This class holds some basic information about a node and custom 
+ * attributes may be added, depending on the algorithms' needs.
+ * @constructor
+ */
 PF.Node = function(x, y) {
+    /**
+     * The x coordinate of the node in grid.
+     * @type number
+     */
     this.x = x;
+    /**
+     * The y coordinate of the node in grid.
+     * @type number
+     */
     this.y = y;
+    /**
+     * Whether this node can be walked through.
+     * @type boolean
+     */
     this.walkable = true;
+    /**
+     * This node's parent node. 
+     * This variable will be used to construct the path after the search is done.
+     */
     this.parent = null;
 };
