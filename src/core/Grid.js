fa6feb0d8b452931743c923c315fc43a3b158b8f
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-09-11 20:24:50 +0800

    add: doc to core/Grid.js

--- src/core/Grid.js
+++ src/core/Grid.js
@@ -1,7 +1,11 @@
 /**
  * The Grid class, which serves as the encapsulation of the nodes on the map.
  * @constructor
- * @param 
+ * @param {integer} numCols Number of columns of the grid.
+ * @param {integer} numRows Number of rows of the grid.
+ * @param {Array.<Array.<integer|boolean>>} [matrix] A 0-1 matrix representing
+ *     the un-walkable status of the nodes(0 or false for walkable). If the
+ *     matrix is not supplied, all the nodes will be walkable.
  */
 PF.Grid = function(numCols, numRows, matrix) {
     this.numCols = numCols;
@@ -10,7 +14,13 @@ PF.Grid = function(numCols, numRows, matrix) {
     this._buildGrid(matrix);
 };
 
-
+/**
+ * Build the grids.
+ * @private
+ * @param {Array.<Array.<integer|boolean>>} [matrix] A 0-1 matrix representing 
+ *     the un-walkable status of the nodes. 
+ * @see PF.Grid
+ */
 PF.Grid.prototype._buildGrid = function(matrix) {
     var i, j, 
         numCols = this.numCols,
@@ -48,17 +58,34 @@ PF.Grid.prototype._buildGrid = function(matrix) {
 };
 
 
+/**
+ * Determine whether the node on the given position is walkable or not.
+ * @param {integer} x The x coordinate of the node.
+ * @param {integer} x The y coordinate of the node.
+ * @return {boolean} The walkability of the node.
+ */
 PF.Grid.prototype.isWalkable = function(x, y) {
     return this.nodes[y][x].walkable;
 };
 
 
+/**
+ * Set the walkability of the node on the given position.
+ * @param {integer} x The x coordinate of the node.
+ * @param {integer} x The y coordinate of the node.
+ */
 PF.Grid.prototype.setWalkable = function(x, y, walkable) {
     this.nodes[y][x].walkable = walkable;
 };
 
 
-PF.Grid.prototype.isValidPos = function(x, y) {
+/**
+ * Determine whether the given position is inside the grid or not.
+ * @param {integer} x The x coordinate of the position.
+ * @param {integer} x The y coordinate of the position.
+ * @return {boolean} Whether the position is inside or not.
+ */
+PF.Grid.prototype.isInside = function(x, y) {
     return x >= 0 && x < this.numCols &&
            y >= 0 && y < this.numRows;
 };
