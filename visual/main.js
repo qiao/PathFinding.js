d4e41919bde9d8cd321ad575eae2bce9770d3cef
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-10-02 10:28:18 +0800

    add: colorization for demo page

--- visual/main.js
+++ visual/main.js
@@ -5,8 +5,13 @@ var Demo = function() {
     this.updateGeometry();
     this.repaint();
 
-    this.finder = new PF.AStarFinder();
+    this.startX = 0;
+    this.startY = 0;
+    this.endX = 0;
+    this.endY = 0;
+    this.finder = new PF.AStarFinder(PF.AStarFinder.euclidean);
     this.queue = [];
+    this.interval = 5;
 };
 
 
@@ -30,13 +35,40 @@ Demo.prototype = {
 
 
     initDispatcher: function() {
+        var colorizeNode,
+            drawParent,
+            self = this;
+
+        colorizeNode = function(x, y, color) {
+            self.rects[y][x].animate({
+                fill: color,
+            }, 20)
+        };
+
+        drawParent = function(x, y, coord) {
+            var path = self.buildSvgPath([[x, y], coord]);
+            self.paper.path(path).attr({
+                opacity: 0.4,
+            });
+        };
+
         this.dispatcher = {
-            opened: '#abf26d',
-            closed: '#61b7cf',
+            opened: function(x, y) {
+                colorizeNode(x, y, '#abf26d');
+            },
+            closed: function(x, y) {
+                colorizeNode(x, y, '#61b7cf');
+            },
+            parent: drawParent,
         };
     },
 
 
+    /**
+     * Re-calculate the geometry of the drawing area.
+     * This method will be called at initiation and 
+     * when the window geometry is modified.
+     */
     updateGeometry: function() {
         var availableWidth,
             availableHeight,
@@ -60,6 +92,7 @@ Demo.prototype = {
 
         // update grid
         this.grid = new PF.Grid(numCols, numRows);
+        console.log('update');
     },
 
 
@@ -93,10 +126,7 @@ Demo.prototype = {
 
 
     start: function() {
-        var finder, path;
-
-        finder = new PF.AStarFinder();
-        path = finder.findPath(0, 0, 10, 10, this.grid);
+        this.path = this.finder.findPath(0, 0, 10, 10, this.grid);
 
         this.replay(); 
     },
@@ -106,16 +136,20 @@ Demo.prototype = {
         var self = this;
 
         this.timer = setInterval(function() {
-            self.step();
-        }, 10)
+            self.step(function() {
+                clearInterval(this.timer);
+                self.drawPath();
+            });
+        }, this.interval);
 
     },
 
     
-    step: function() {
+    step: function(callback) {
         var queue, front,
             rects,
             dispatcher,
+            func,
             x, y,
             attr, value,
 
@@ -124,7 +158,7 @@ Demo.prototype = {
         queue = this.queue;
 
         if (!queue.length) {
-            clearInterval(this.timer);
+            callback();
             return;
         }
         front = queue.shift();
@@ -133,12 +167,32 @@ Demo.prototype = {
         attr = front[2][0];
         value = front[2][1];
 
-        color = dispatcher[attr];
-        if (color) {
-            rects[y][x].animate({
-                fill: color,
-            }, 50);
+        func = dispatcher[attr];
+        if (func) {
+            func(x, y, value);
         }
+    },
+
+
+    drawPath: function() {
+        var path = this.buildSvgPath(this.path);
+        this.paper.path(path).attr({
+            stroke: 'yellow'
+        });
+    },
+
+    
+    buildSvgPath: function(path) {
+        var i, strs = [], size = this.gridSize;
+
+        strs.push('M' + (path[0][0] * size + size / 2) + ' ' 
+                  + (path[0][1] * size + size / 2));
+        for (i = 1; i < path.length; ++i) {
+            strs.push('L' + (path[i][0] * size + size / 2) + ' ' 
+                    + (path[i][1] * size + size / 2));
+        }
+
+        return strs.join('');
     },
 
 };
