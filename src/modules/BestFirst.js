c04659b907aff90e6a6e7fbd3a21a8c3bec56ec1
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-10-06 18:36:38 +0800

    add: custom heuristic for best-first-search

--- src/modules/BestFirst.js
+++ src/modules/BestFirst.js
@@ -4,10 +4,13 @@
  * @extends PF.AStarFinder
  * @param {boolean} allowDiagonal - Whether diagonal movement is allowed.
  */
-PF.BestFirstFinder = function(allowDiagonal) {
-    PF.AStarFinder.call(this, allowDiagonal, function(dx, dy) {
-        return Math.sqrt(dx * dx + dy * dy) * 1000000;
-    })
+PF.BestFirstFinder = function(allowDiagonal, heuristic) {
+    PF.AStarFinder.call(this, allowDiagonal, heuristic);
+
+    var orig = this.heuristic;
+    this.heuristic = function(dx, dy) {
+        return orig(dx, dy) * 1000000;
+    };
 };
 
 PF.BestFirstFinder.prototype = new PF.AStarFinder();
