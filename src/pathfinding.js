5fce66a6e9f30054bc8cf58126d81ce939be93bd
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-01-30 21:23:31 +0800

    add: bundled export

--- src/pathfinding.js
+++ src/pathfinding.js
@@ -1,4 +1,15 @@
 module.exports = {
-    'Grid'         : require('./core/grid').Grid;
-    'BaseFinder': require('./core')
+    'Node'                 : require('./core/node'),
+    'Grid'                 : require('./core/grid'),
+    'Heap'                 : require('./core/heap'),
+    'Heuristic'            : require('./core/heuristic'),
+    'BaseFinder'           : require('./finders/base'),
+    'AStarFinder'          : require('./finders/astar'),
+    'BestFirstFinder'      : require('./finders/best_first'),
+    'BreadthFirstFinder'   : require('./finders/breadth_first'),
+    'DijkstraFinder'       : require('./finders/dijkstra'),
+    'BiAStarFinder'        : require('./finders/bi_astar'),
+    'BiBestFirstFinder'    : require('./finders/bi_best_first'),
+    'BiBreadthFirstFinder' : require('./finders/bi_breadth_first'),
+    'BiDijkstraFinder'     : require('./finders/bi_dijkstra')
 };
