d1115879e62290d030354f22989d7aed834ab32d
Xueqiao Xu <xueqiaoxu@gmail.com>
2012-04-28 00:20:12 +0800

    mod: rewrite BiBestFirstFinder

--- src/finders/BiBestFirstFinder.js
+++ src/finders/BiBestFirstFinder.js
@@ -1,12 +1,12 @@
-var BiAStarFinder = require('./bi_astar');
+var BiAStarFinder = require('./BiAStarFinder');
 
 /**
  * Bi-direcitional Best-First-Search path-finder.
  * @constructor
  * @extends BiAStarFinder
- * @param {boolean} opt - 
- *     opt.allowDiagonal: Whether diagonal movement is allowed.
- *     [opt.heuristic]: Heuristic function being used to estimate the distance
+ * @param {object} opt
+ * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
  *     (defaults to manhattan).
  */
 function BiBestFirstFinder(opt) {
@@ -16,7 +16,7 @@ function BiBestFirstFinder(opt) {
     this.heuristic = function(dx, dy) {
         return orig(dx, dy) * 1000000;
     };
-};
+}
 
 BiBestFirstFinder.prototype = new BiAStarFinder();
 BiBestFirstFinder.prototype.constructor = BiBestFirstFinder;
