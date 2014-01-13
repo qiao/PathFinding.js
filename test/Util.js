7af6938fe939243cf2f043ef746087bb03957744
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-01-13 13:11:08 -0800

    add: test case for utility functions

---
+++ test/Util.js
@@ -0,0 +1,31 @@
+var PF = require('..');
+
+describe('Utility functions', function () {
+    describe('interpolate', function () {
+        it('should return the interpolated path', function () {
+            PF.Util.interpolate(0, 1, 0, 4).should.eql([
+                [0, 1], [0, 2], [0, 3], [0, 4]
+            ]);
+        });
+    });
+
+    describe('expandPath', function () {
+        it('should return an empty array given an empty array', function () {
+            PF.Util.expandPath([]).should.eql([]);
+        });
+
+        it('should return the expanded path', function () {
+            PF.Util.expandPath([
+                [0, 1], [0, 4]
+            ]).should.eql([
+                [0, 1], [0, 2], [0, 3], [0, 4]
+            ]);
+
+            PF.Util.expandPath([
+                [0, 1], [0, 4], [2, 6]
+            ]).should.eql([
+                [0, 1], [0, 2], [0, 3], [0, 4], [1, 5], [2, 6]
+            ]);
+        });
+    });
+});
