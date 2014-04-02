dd17a5a11baebb2e848c8ad2d2281077b0bca847
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-04-02 10:06:41 -0700

    Revert "Allow the Matrix to be used for non-walkable grids."
    This reverts commit 9dc2bd3aabe992a544d330ed9be1a896473cccb0.

--- src/core/Grid.js
+++ src/core/Grid.js
@@ -56,15 +56,12 @@ Grid.prototype._buildNodes = function(width, height, matrix) {
         throw new Error('Matrix size does not fit');
     }
 
-    // Thje matrix is used to flip nodes from the default, so it doesn't have to mutate every node.
-    var matrixWalkable = !walkable;
-
     for (i = 0; i < height; ++i) {
         for (j = 0; j < width; ++j) {
-            if (matrix[i][j] != walkable) {
+            if (matrix[i][j]) {
                 // 0, false, null will be walkable
                 // while others will be un-walkable
-                nodes[i][j].walkable = matrixWalkable;
+                nodes[i][j].walkable = false;
             }
         }
     }
