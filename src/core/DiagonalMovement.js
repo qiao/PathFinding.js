028466e823b5f78b28d348cd6d992f5b836ffe83
Roman <romsa9@gmail.com>
2015-04-08 22:47:14 -0400

    New diagonal movement option: only when obstacles present.

--- src/core/DiagonalMovement.js
+++ src/core/DiagonalMovement.js
@@ -2,7 +2,8 @@ var DiagonalMovement = {
     Always: 1,
     Never: 2,
     IfAtMostOneObstacle: 3,
-    OnlyWhenNoObstacles: 4
+    OnlyWhenNoObstacles: 4,
+    OnlyWhenObstacles: 5,
 };
 
 module.exports = DiagonalMovement;
