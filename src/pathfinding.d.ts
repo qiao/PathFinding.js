cee33cdc6bfd8638bd31aa5ebf0570224afaf08c
Matthew James Davis <davis.matthewjames@gmail.com>
2016-06-03 16:09:36 -0400

    feat(pathfinding.d.ts): Add typings
    Add a basic module declaration file to add basic intellisense and to avoid compiler errors when using in a TypeScript project.

---
+++ src/pathfinding.d.ts
@@ -0,0 +1,19 @@
+/* */ 
+declare module 'pathfinding' {
+  export var Heap: any;
+  export var Node: any;
+  export var Grid: any;
+  export var Util: any;
+  export var DiagonalMovement: any;
+  export var Heuristic: any;
+  export var AStarFinder: any;
+  export var BestFirstFinder: any;
+  export var BreadthFirstFinder: any;
+  export var DijkstraFinder: any;
+  export var BiAStarFinder: any;
+  export var BiBestFirstFinder: any;
+  export var BiBreadthFirstFinder: any;
+  export var BiDijkstraFinder: any;
+  export var IDAStarFinder: any;
+  export var JumpPointFinder: any;
+}
