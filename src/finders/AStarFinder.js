aa8371d930af44adc07dc8e03d073d6f3635e1f6
Matthieu Lemoine <mlemoine@yumii.fr>
2017-07-17 17:48:03 +0200

    :sparkles: Unit size in AStarFinder

--- src/finders/AStarFinder.js
+++ src/finders/AStarFinder.js
@@ -9,13 +9,14 @@ var DiagonalMovement = require('../core/DiagonalMovement');
  * @param {Object} opt
  * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
  *     Deprecated, use diagonalMovement instead.
- * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching 
+ * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
  *     block corners. Deprecated, use diagonalMovement instead.
  * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
  * @param {function} opt.heuristic Heuristic function to estimate the distance
  *     (defaults to manhattan).
  * @param {number} opt.weight Weight to apply to the heuristic to allow for
  *     suboptimal paths, in order to speed up the search.
+ * @param {number} opt.unitSize Size of the moving unit
  */
 function AStarFinder(opt) {
     opt = opt || {};
@@ -24,6 +25,8 @@ function AStarFinder(opt) {
     this.heuristic = opt.heuristic || Heuristic.manhattan;
     this.weight = opt.weight || 1;
     this.diagonalMovement = opt.diagonalMovement;
+    this.unitSize = opt.unitSize || 1;
+    this.withClearance = this.unitSize !== 1;
 
     if (!this.diagonalMovement) {
         if (!this.allowDiagonal) {
@@ -63,6 +66,10 @@ AStarFinder.prototype.findPath = function(startX, startY, endX, endY, grid) {
         abs = Math.abs, SQRT2 = Math.SQRT2,
         node, neighbors, neighbor, i, l, x, y, ng;
 
+
+    if (this.withClearance) {
+        this.calculateClearance(grid);
+    }
     // set the `g` and `f` value of the start node to be 0
     startNode.g = 0;
     startNode.f = 0;
@@ -107,7 +114,9 @@ AStarFinder.prototype.findPath = function(startX, startY, endX, endY, grid) {
                 neighbor.parent = node;
 
                 if (!neighbor.opened) {
-                    openList.push(neighbor);
+                    if (!this.withClearance || neighbor.clearance >= this.unitSize) {
+                        openList.push(neighbor);
+                    }
                     neighbor.opened = true;
                 } else {
                     // the neighbor can be reached with smaller cost.
@@ -122,5 +131,42 @@ AStarFinder.prototype.findPath = function(startX, startY, endX, endY, grid) {
     // fail to find the path
     return [];
 };
+
+/**
+ * Generate the clearance grid.
+ */
+AStarFinder.prototype.calculateClearance = function(grid) {
+    // Recursive from the bottom right
+    for (var x = grid.width-1; x>=0; x--) {
+        for(var y = grid.height - 1; y>=0; y--) {
+            var n = grid.getNodeAt(x, y);
+            this.annotateNode(n, grid);
+        }
+    }
+};
+
+/**
+ * Annotate a node with clearance value.
+ */
+AStarFinder.prototype.annotateNode = function(n, grid) {
+    if (!n.walkable) {
+        return 0;
+    }
+    var x = n.x;
+    var y = n.y;
+    // On border
+    if (x + 1 >= grid.width || y + 1 >= grid.height) {
+        n.clearance = 1;
+    } else {
+        // Adjacent neighbours
+        var adj1 = grid.getNodeAt(x+1, y+1);
+        var adj2 = grid.getNodeAt(x+1,y);
+        var adj3 = grid.getNodeAt(x,y+1);
+        if (adj1 && adj2 && adj3) {
+            n.clearance = Math.min(adj1.clearance, adj2.clearance, adj3.clearance) + 1;
+        }
+    }
+};
+
 
 module.exports = AStarFinder;
