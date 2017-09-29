fa2660e00b5eb7e7b0a49f701c3717d413dcf1ab
James Brown <jambrown@adobe.com>
2017-09-29 14:30:44 -0600

    Added support to grid / node for weights per node. Added usage to AStarFinder to use the new weights.  Weights above 1 (the default) make a node less desirable (cost more expensive) to use but not unwalkable.  Weights less than 1 make a node more desirable (cost less expensive) to use.

--- src/finders/AStarFinder.js
+++ src/finders/AStarFinder.js
@@ -97,7 +97,9 @@ AStarFinder.prototype.findPath = function(startX, startY, endX, endY, grid) {
             // get the distance between current node and the neighbor
             // and calculate the next g score
             ng = node.g + ((x - node.x === 0 || y - node.y === 0) ? 1 : SQRT2);
-
+            
+            ng *= neighbor.weight;
+            
             // check if the neighbor has not been inspected yet, or
             // can be reached with smaller cost from the current node
             if (!neighbor.opened || ng < neighbor.g) {
